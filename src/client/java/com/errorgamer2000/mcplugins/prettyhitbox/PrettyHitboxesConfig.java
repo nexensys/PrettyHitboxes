31a47486d5f340b9d5837ba4cc2a2a5e13d413a2
ErrorGamer2000 <errorgamer2000@outlook.com>
2023-06-02 19:42:16 -0600

    Fix ModMenu annotations

--- src/client/java/com/errorgamer2000/mcplugins/prettyhitbox/PrettyHitboxesConfig.java
+++ src/client/java/com/errorgamer2000/mcplugins/prettyhitbox/PrettyHitboxesConfig.java
@@ -6,24 +6,45 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "pretty-hitboxes")
 public class PrettyHitboxesConfig implements ConfigData {
+    @ConfigEntry.Category("Features")
+    @ConfigEntry.Gui.Tooltip
     public boolean hitboxesEnabledByDefault = false;
 
+    @ConfigEntry.Category("Features")
+    @ConfigEntry.Gui.Tooltip
     public boolean hideBigDragonBox = false;
-
+    @ConfigEntry.Category("Features")
+    @ConfigEntry.Gui.Tooltip
     public boolean showBoundingBox = true;
+    @ConfigEntry.Category("Features")
+    @ConfigEntry.Gui.Tooltip
     public boolean showEyeHeight = true;
+    @ConfigEntry.Category("Features")
+    @ConfigEntry.Gui.Tooltip
     public boolean showEntityRotationVector = true;
+    @ConfigEntry.Category("Features")
+    @ConfigEntry.Gui.Tooltip
     public boolean differentColorWhenTargeted = false;
 
+    @ConfigEntry.Category("Colors")
     @ConfigEntry.Gui.CollapsibleObject
+    @ConfigEntry.Gui.Tooltip
     public Color boundingBoxColor = new Color(255, 255, 255);
+    @ConfigEntry.Category("Colors")
     @ConfigEntry.Gui.CollapsibleObject
+    @ConfigEntry.Gui.Tooltip
     public Color dragonPartColor = new Color(0, 255, 0);
+    @ConfigEntry.Category("Colors")
     @ConfigEntry.Gui.CollapsibleObject
+    @ConfigEntry.Gui.Tooltip
     public Color eyeHeightColor = new Color(255, 0, 0);
+    @ConfigEntry.Category("Colors")
     @ConfigEntry.Gui.CollapsibleObject
+    @ConfigEntry.Gui.Tooltip
     public Color entityRotationVectorColor = new Color(0, 0, 255);
+    @ConfigEntry.Category("Colors")
     @ConfigEntry.Gui.CollapsibleObject
+    @ConfigEntry.Gui.Tooltip
     public Color entityTargetedColor = new Color(100, 100, 100);
 
 
@@ -33,8 +54,11 @@ public class PrettyHitboxesConfig implements ConfigData {
             this.green = g;
             this.blue = b;
         }
+        @ConfigEntry.Gui.Tooltip
         public int red;
+        @ConfigEntry.Gui.Tooltip
         public int green;
+        @ConfigEntry.Gui.Tooltip
         public int blue;
         public int alpha = 100;
     }
